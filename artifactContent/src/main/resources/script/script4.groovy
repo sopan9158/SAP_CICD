af3e261a43527cb43f57c2ec673bbfec7932a789
sopan9158 <31924480+sopan9158@users.noreply.github.com>
2022-02-18 10:48:13 +0530

    Delete artifactContent directory

--- artifactContent/src/main/resources/script/script4.groovy
+++
@@ -1,9 +0,0 @@
-import com.sap.gateway.ip.core.customdev.util.Message;
-import java.util.HashMap;
-def Message processData(Message message) {
-    //Body 
-    def var1 = ","
-       def body = message.getBody(java.lang.String) as String;
-        message.setBody(body.replaceAll(var1,'","'))
-       return message;
-}
